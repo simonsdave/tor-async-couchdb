40735da85788761b2199696fb7c3dc5672d70c18
Dave Simons <simonsdave@gmail.com>
2015-04-13 07:06:10 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#
+# to build the distrubution @ tor_async_couchdb-1.0.0.tar.gz
+#
+#   >git clone https://github.com/simonsdave/tor-async-couchdb.git
+#   >cd tor-async-couchdb
+#   >source cfg4dev
+#   >python setup.py sdist --formats=gztar
+#
+from setuptools import setup
+
+setup(
+    name="tor_async_couchdb",
+    packages=[
+        "tor_async_couchdb",
+    ],
+    install_requires=[
+        "python-keyczar==0.715",
+    ],
+    version="1.0.0",
+    description="Tornado Async Client for CouchDB",
+    author="Dave Simons",
+    author_email="simonsdave@gmail.com",
+    url="https://github.com/simonsdave/tor-async-couchdb"
+)
