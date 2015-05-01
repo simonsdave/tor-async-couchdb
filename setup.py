f449fa124f9441715a3fc07b0b32a66bd3ab37de
Dave Simons <simonsdave@gmail.com>
2015-05-01 15:45:36 -0400

    tor_async_couchdb version # availalbe @ runtime

--- setup.py
+++ setup.py
@@ -6,7 +6,33 @@
 #   >source cfg4dev
 #   >python setup.py sdist --formats=gztar
 #
+import re
+
 from setuptools import setup
+
+#
+# this approach used below to determine ```version``` was inspired by
+# https://github.com/kennethreitz/requests/blob/master/setup.py#L31
+#
+# why this complexity? wanted version number to be available in the
+# a runtime.
+#
+# the code below assumes the distribution is being built with the
+# current directory being the directory in which setup.py is stored
+# which should be totally fine 99.9% of the time. not going to add
+# the coode complexity to deal with other scenarios
+#
+reg_ex_pattern = r"__version__\s*=\s*['\"](?P<version>[^'\"]*)['\"]"
+reg_ex = re.compile(reg_ex_pattern)
+version = ""
+with open("tor_async_couchdb/__init__.py", "r") as fd:
+    for line in fd:
+        match = reg_ex.match(line)
+        if match:
+            version = match.group("version")
+            break
+if not version:
+    raise Exception("Can't locate tor_async_councdb's version number")
 
 setup(
     name="tor_async_couchdb",
@@ -16,7 +42,7 @@ setup(
     install_requires=[
         "python-keyczar==0.715",
     ],
-    version="0.9.2",
+    version=version,
     description="Tornado Async Client for CouchDB",
     author="Dave Simons",
     author_email="simonsdave@gmail.com",
