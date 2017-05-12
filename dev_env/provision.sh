5114c6ace9d4237bb9e4979b118a3ea8f5a87baf
Dave Simons <simonsdave@gmail.com>
2017-05-12 18:10:56 -0400

    restructure dev env provisioning

--- dev_env/provision.sh
+++ dev_env/provision.sh
@@ -8,8 +8,34 @@ set -e
 
 apt-get update -y
 
+#
+# install docker
+#
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine
+usermod -aG docker vagrant
+service docker restart
+
+#
+# install and configure git
+#
 apt-get install -y git
 
+if [ $# == 2 ]; then
+    su - vagrant -c "git config --global user.name \"${1:-}\""
+    su - vagrant -c "git config --global user.email \"${2:-}\""
+fi
+
+su vagrant <<'EOF'
+echo 'export VISUAL=vim' >> ~/.profile
+echo 'export EDITOR="$VISUAL"' >> ~/.profile
+EOF
+
+#
+#
+#
 apt-get install -y python-virtualenv
 apt-get install -y python-dev
 apt-get build-dep -y python-crypto
@@ -34,19 +60,52 @@ cp /vagrant/local.ini /etc/couchdb/local.ini
 chown couchdb:couchdb /etc/couchdb/local.ini
 service couchdb restart
 
-curl -s -L --output /usr/local/bin/jq 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64'
-chown root.root /usr/local/bin/jq
-chmod a+x /usr/local/bin/jq
+#
+# jq is just so generally useful
+#
+JQ_SOURCE=https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+JQ_BIN=/usr/local/bin/jq
+curl -s -L --output "$JQ_BIN" "$JQ_SOURCE"
+chown root.root "$JQ_BIN"
+chmod a+x "$JQ_BIN"
 
-cp /vagrant/.vimrc ~vagrant/.vimrc
-chown vagrant:vagrant ~vagrant/.vimrc
+#
+# customize vim
+#
+su vagrant <<'EOF'
+echo 'set ruler' > ~/.vimrc
+echo 'set hlsearch' >> ~/.vimrc
+echo 'filetype plugin on' >> ~/.vimrc
+echo 'filetype indent on' >> ~/.vimrc
+echo 'set ts=4' >> ~/.vimrc
+echo 'set sw=4' >> ~/.vimrc
+echo 'set expandtab' >> ~/.vimrc
+echo 'set encoding=UTF8' >> ~/.vimrc
+echo 'colorscheme koehler' >> ~/.vimrc
+echo 'syntax on' >> ~/.vimrc
 
-echo 'export VISUAL=vim' >> ~vagrant/.profile
-echo 'export EDITOR="$VISUAL"' >> ~vagrant/.profile
+echo 'au BufNewFile,BufRead *.sh set filetype=shell' >> ~/.vimrc
+echo 'autocmd Filetype shell setlocal expandtab tabstop=4 shiftwidth=4' >> ~/.vimrc
 
-if [ $# == 2 ]; then
-    su - vagrant -c "git config --global user.name \"${1:-}\""
-    su - vagrant -c "git config --global user.email \"${2:-}\""
-fi
+echo 'au BufNewFile,BufRead *.json set filetype=json' >> ~/.vimrc
+echo 'autocmd FileType json setlocal expandtab tabstop=4 shiftwidth=4' >> ~/.vimrc
+
+echo 'au BufNewFile,BufRead *.py set filetype=python' >> ~/.vimrc
+echo 'autocmd FileType python setlocal expandtab tabstop=4 shiftwidth=4' >> ~/.vimrc
+
+echo 'au BufNewFile,BufRead *.raml set filetype=raml' >> ~/.vimrc
+echo 'autocmd FileType raml setlocal expandtab tabstop=2 shiftwidth=2' >> ~/.vimrc
+
+echo 'au BufNewFile,BufRead *.yaml set filetype=yaml' >> ~/.vimrc
+echo 'autocmd FileType yaml setlocal expandtab tabstop=2 shiftwidth=2' >> ~/.vimrc
+
+echo 'au BufNewFile,BufRead *.js set filetype=javascript' >> ~/.vimrc
+echo 'autocmd FileType javascript setlocal expandtab tabstop=2 shiftwidth=2' >> ~/.vimrc
+
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+sed -i '1s|^|execute pathogen#infect()\n|' ~/.vimrc
+EOF
 
 exit 0
