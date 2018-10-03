0e7dc2a514f202dd857585f5bd2f2f3f782f2ae1
simonsdave <simonsdave@gmail.com>
2018-10-02 22:40:19 -0500

    some shell linting

--- bin/kill-and-remove-all-docker-containers.sh
+++ bin/kill-and-remove-all-docker-containers.sh
@@ -5,12 +5,12 @@
 
 set -e
 
-for CONTAINER_ID in `docker ps -q`; do
-    docker kill  $CONTAINER_ID
+for CONTAINER_ID in $(docker ps -q); do
+    docker kill "$CONTAINER_ID"
 done
 
-for CONTAINER_ID in `docker ps -a -q`; do
-    docker rm  $CONTAINER_ID
+for CONTAINER_ID in $(docker ps -a -q); do
+    docker rm "$CONTAINER_ID"
 done
 
 exit 0
