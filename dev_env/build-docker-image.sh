f3c68baed2f6b38e1c5473032d9a1327eaac6bb9
simonsdave <simonsdave@gmail.com>
2018-10-03 05:53:02 -0500

    add samples requirements.txt docker image dev env

--- dev_env/build-docker-image.sh
+++ dev_env/build-docker-image.sh
@@ -21,6 +21,8 @@ cp "$PROJECT_HOME_DIR/requirements.txt" "$CONTEXT_DIR/."
 cp "$PROJECT_HOME_DIR/setup.py" "$CONTEXT_DIR/."
 mkdir "$CONTEXT_DIR/tor_async_couchdb"
 cp "$PROJECT_HOME_DIR/tor_async_couchdb/__init__.py" "$CONTEXT_DIR/tor_async_couchdb/."
+mkdir "$CONTEXT_DIR/samples"
+cp "$PROJECT_HOME_DIR/samples/requirements.txt" "$CONTEXT_DIR/samples/."
 
 DEV_ENV_VERSION=$(cat "$SCRIPT_DIR_NAME/dev-env-version.txt")
 
